f1a24e8335166a1ed2c3690d18849d0282f7483e
Felipe Roque <feliperamosroque@gmail.com>
2020-06-25 14:10:33 -0300

    Projeto criado

---
+++ src/main/java/br/com/felipe/curso/ProjetomongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipe.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongodbApplication.class, args);
+	}
+
+}
