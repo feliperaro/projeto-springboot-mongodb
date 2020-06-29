17253b6ae21ede28921b084c54c7b7ff1c3200c4
Felipe Roque <feliperamosroque@gmail.com>
2020-06-29 18:15:23 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/br/com/felipe/curso/config/TestConfig.java
+++ src/main/java/br/com/felipe/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.felipe.curso.domain.Post;
 import br.com.felipe.curso.domain.User;
+import br.com.felipe.curso.dto.AuthorDTO;
 import br.com.felipe.curso.repository.PostRepository;
 import br.com.felipe.curso.repository.UserRepository;
 
@@ -18,30 +19,29 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Marrom", "maria@gmail.com");
 		User alex = new User(null, "Alex Verde", "alex@gmail.com");
 		User bob = new User(null, "Bob Cinza", "bob@gmail.com");
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
-		Post post1 = new Post(null, sdf.parse("29/06/2020"), "Partiu viagem!", "Vou viajar para o Rio de Janeiro. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("20/06/2020"), "Bom dia", "Acordei feliz hoje...", maria);
+
+		Post post1 = new Post(null, sdf.parse("29/06/2020"), "Partiu viagem!",
+				"Vou viajar para o Rio de Janeiro. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("20/06/2020"), "Bom dia", "Acordei feliz hoje...", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
-		
 	}
-
 }
