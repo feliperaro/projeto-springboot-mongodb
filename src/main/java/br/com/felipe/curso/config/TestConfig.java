6453360325ea458ce27c26864662593861460ec6
Felipe Roque <feliperamosroque@gmail.com>
2020-07-01 16:29:21 -0300

    Acrescentando comentarios aos posts

--- src/main/java/br/com/felipe/curso/config/TestConfig.java
+++ src/main/java/br/com/felipe/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.felipe.curso.domain.Post;
 import br.com.felipe.curso.domain.User;
 import br.com.felipe.curso.dto.AuthorDTO;
+import br.com.felipe.curso.dto.CommentDTO;
 import br.com.felipe.curso.repository.PostRepository;
 import br.com.felipe.curso.repository.UserRepository;
 
@@ -42,6 +43,13 @@ public class TestConfig implements CommandLineRunner {
 				"Vou viajar para o Rio de Janeiro. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("20/06/2020"), "Bom dia", "Acordei feliz hoje...", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
